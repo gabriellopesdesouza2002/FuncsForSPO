c39005816a1be95f2b96d56928ebdab668bc6948
githubpaycon <githubpaycon@gmail.com>
2022-07-26 18:36:56 -0300

    current_version=0.0.3.11

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='FuncsForSPO',
-    version='0.0.3.10',
+    version='0.0.3.11',
     url='https://github.com/githubpaycon/FuncsForSPO',
     license='MIT License',
     author='Gabriel Lopes de Souza',
